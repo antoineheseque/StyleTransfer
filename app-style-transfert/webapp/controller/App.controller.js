6413f1e223c678aed316ced2118ab3c959ddfc7b
Imane-C <imane.choukhii@hotmail.fr>
2021-10-21 11:39:35 +0200

    lkqsfnc

--- app-style-transfert/webapp/controller/App.controller.js
+++ app-style-transfert/webapp/controller/App.controller.js
@@ -31,103 +31,120 @@ sap.ui.define([
 						"id": "por-amor-al-arte",
 						"title": "Por amor al arte",
 						"artist": "Julien Raynaud",
-						"format": "jpg"
+						"format": "jpg",
+						"link": "https://1.bp.blogspot.com/-FpJf3U_amU8/Xb63rMBTGtI/AAAAAAAIrlI/f0DYf4ix8zwYFDnQWHpxzZ3QguZ3NfxZACLcBGAsYHQ/s1600/Julien%2BRaynaud%2B%25282%2529.jpg"
 					},
 					{
 						"id": "twilight",
 						"title": "Twilight",
 						"artist": "Hale Woodruff",
-						"format": "jpg"
+						"format": "jpg",
+						"link": "https://i.pinimg.com/originals/df/0c/fd/df0cfd2ca3ecb465beb1e9c9bb74a3c7.jpg"
 					},
 					{
 						"id": "nuit-etoilee",
 						"title": "Nuit étoilée",
 						"artist": "Van Gogh",
-						"format": "jpg"
+						"format": "jpg",
+						"link": "https://www.kazoart.com/blog/wp-content/uploads/2017/03/van-gogh-la-nuit-etoilee-1.jpg"
 					},
 					{
 						"id": "guernica",
 						"title": "Guernica",
 						"artist": "Pablo Picasso",
-						"format": "png"
+						"format": "png",
+						"link": "https://www.connaissancedesarts.com/wp-content/uploads/2021/03/guernica-onu-1.jpg"
 					},
 					{
 						"id": "diptyque-marilyn",
 						"title": "Diptyque Marilyn",
 						"artist": "Andy Warhol",
-						"format": "png"
+						"format": "png",
+						"link": "https://img2.freepng.fr/20180524/rvl/kisspng-marilyn-monroe-marilyn-diptych-campbell-s-soup-can-5b078864d85e17.9273665915272203248863.jpg"
 					},
 					{
 						"id": "compliment",
 						"title": "Compliment",
 						"artist": "Frantisek Kupka",
-						"format": "png"
+						"format": "png",
+						"link": "https://blog.artsper.com/wp-content/uploads/2018/12/thumb_large.jpg"
 					},
 					{
 						"id": "abstract",
 						"title": "Abstract",
 						"artist": "Unknown",
-						"format": "png"
+						"format": "jpg",
+						"link": "https://www.artmajeur.com/medias/standard/s/c/schreiber-patrice/artwork/10929673_100-1032.jpg"
 					},
 					{
 						"id": "the-great-wave-of-kanagawa",
 						"title": "The great wave of Kanagawa",
 						"artist": "Katsuki Hokusai",
-						"format": "png"
+						"format": "png",
+						"link": "https://upload.wikimedia.org/wikipedia/commons/a/a5/Tsunami_by_hokusai_19th_century.jpg"
 					},
 					{
 						"id": "autoportrait-a-la-casquette",
 						"title": "Autoportrait à la casquette",
 						"artist": "André Derain",
-						"format": "png"
+						"format": "png",
+						"link": "https://lh3.googleusercontent.com/proxy/whhXIF0neCoPNZ3wWCbZYZ_bbqT3021_4c7Ray-mrdgPgahfmWMGMv0MlWU8onbqWm0F0S7O8OS_qOWZ-2p-NXSKdtdkR_IJmiv6meTRaxvCTyjiBbLG2NOXmQbl8IMP-w"
 					},
 					{
 						"id": "lestaque",
 						"title": "L'Estaque",
 						"artist": "André Derain",
-						"format": "png"
+						"format": "png",
+						"link": "https://img.over-blog-kiwi.com/1/47/74/15/20190505/ob_b8de79_derain-theturningroad-l-estaque-1906.jpg"
 					},
 					{
 						"id": "contemporain",
 						"title": "Contemporain",
 						"artist": "Unknown",
-						"format": "png"
+						"format": "png",
+						"link": "https://urdla.com/blog/wp-content/uploads/2019/08/deleglise0710-copie-1.jpg"
 					},
 					{
 						"id": "lichtung",
 						"title": "Lichtung",
 						"artist": "Dagmar Vogt",
-						"format": "png"
+						"format": "png",
+						"link": "https://cdn.singulart.com/artworks/pictures/cropped/323/481/fhd/481_db1778b9c154d42790124de7d94f8a7b.jpeg"
 					},
 					{
 						"id": "vue-de-collioure",
 						"title": "Vue de Collioure",
 						"artist": "André Derain",
-						"format": "png"
+						"format": "png",
+						"link": "https://live.staticflickr.com/7835/32490437447_09729cb799_b.jpg"
 					},
 					{
 						"id": "lever-du-soleil",
 						"title": "Lever du soleil",
 						"artist": "Otto Dix",
-						"format": "png"
+						"format": "png",
+						"link": "https://wahooart.com/Art.nsf/O/8LT3CL/$File/Otto-Dix-Sunrise.jpg"
 					},
 					{
 						"id": "girly",
 						"title": "Girly",
-						"artist": "Jean-Nicolas Gaillard",
-						"format": "png"
+						"artist": "Unknown",
+						"format": "jpg",
+						"link": "https://wallpaperaccess.com/full/958076.jpg"
 					},
 					{
 						"id": "color-talk",
 						"title": "Color talk",
 						"artist": "Picasso",
-						"format": "png"
+						"format": "png",
+						"link": "https://m.media-amazon.com/images/I/51F7Watf59L._AC_SX450_.jpg"
 					},
 					{
 						"id": "sans-titre",
 						"title": "Sans titre",
 						"artist": "JonOne",
-						"format": "png"
+						"format": "png",
+						"link": "https://galerie-provost-hacker.com/wp-content/uploads/2020/05/bd-2019-le-depart-acrylique-et-encre-sur-toile-150-x-142-cm-jonone-2019-repro-706-460x460-1.jpg"
 					}
 				]
 
@@ -151,28 +168,43 @@ sap.ui.define([
 
 		},
 
+		onChooseImg: function(){
+
+			var url = this.getView().byId("inputUrl").getValue();
+			this.onValidateUrl(url);
+		},
+
+		onValidateUrl: function(url){
+			if(url.match(/\.(jpeg|jpg|png)$/) != null){
+				this.onUploadImage(url);
+			}else{
+				this.getView().byId("inputUrl").setValue("");
+				MessageToast.show("Invalid link!");
+			}
+		},
+
 		onReset: function (){
 			window.location.reload();
 		},
 
-		onUploadImage: function(){
+		onUploadImage: function(url){
 
-			var filesId = this.getView().byId("imageUploader").sId + "-fu";
-			var files = document.getElementById(filesId).files;
-			var formData = new FormData();
-			var xhttp = new XMLHttpRequest();
+			// var filesId = this.getView().byId("imageUploader").sId + "-fu";
+			// var files = document.getElementById(filesId).files;
+			// var formData = new FormData();
+			// var xhttp = new XMLHttpRequest();
 
+			// formData.append("file", files[0]);
 
-			formData.append("file", files[0]);
+			// // Set POST method and ajax file path
+			// xhttp.open("POST", "../webapp/resources/php/Upload.php", true);
 
-			// Set POST method and ajax file path
-			xhttp.open("POST", "../webapp/resources/php/Upload.php", true);
-
-			// Send request with data
-			xhttp.send(formData);
-
+			// // Send request with data
+			// xhttp.send(formData);
+			
 			//set elements visble 
-			this.getView().byId('imgTile').setBackgroundImage('resources/img/'+files[0].name);
+			this.getView().byId('imgTile').setBackgroundImage(url);
+			//this.getView().byId('imgTile').setBackgroundImage('resources/img/'+files[0].name);
 			this.getView().byId('imgTile').setVisible(true);
 			this.getView().byId('imageUploader').setVisible(false);
 
@@ -190,6 +222,7 @@ sap.ui.define([
 		onSelectArtist: function(oEvent){
 
 			var sSourceImg = oEvent.getSource().mProperties.backgroundImage;
+			var sLink = oEvent.getSource().mProperties.ariaLabel;
 			var sArtist = oEvent.getSource().mProperties.imageDescription;
 
 			var oArtworks =  this.getView().byId('artworks').getModel().getData();
@@ -202,6 +235,7 @@ sap.ui.define([
 
 			this.getView().byId('styleChoosen').setBackgroundImage(sSourceImg);
 			this.getView().byId('styleChoosen').setImageDescription(sArtist);
+			this.getView().byId('styleChoosen').setAriaLabel(sLink);
 			this.getView().byId('styleChoosen').setVisible(true);
 			this.getView().byId('artistChoosen').setText(sArtist);
 			this.getView().byId('artistChoosen').setVisible(true);
@@ -228,14 +262,13 @@ sap.ui.define([
 		},
 
 		handleCloseButton: function (oEvent) {
-			// note: We don't need to chain to the _pPopover promise, since this event-handler
-			// is only called from within the loaded dialog itself.
+
 			this.byId("myPopover").close();
 		},
 
 
 		handleUploadComplete: function(oEvent) {
-			// var sResponse = oEvent.getParameter("response"),
+
 			var sResponse = oEvent.getParameter("status"),
 				iHttpStatusCode = parseInt(/\d{3}/.exec(sResponse)[0]),
 				
@@ -257,21 +290,32 @@ sap.ui.define([
 									aFileTypes.join(", "));
 		},
 
-		onProcess: function(){
+		onProcess: function(oEvent){
 
-			$.ajax({
-				type: "POST",
-				url: "./resources/python/process.py",
-				success: function(data){
-					var response = JSON.stringify(data);
-				}
-			});
-			//
-			
+			var inputStyleLink = this.getView().byId('styleChoosen').getAriaLabel();
+			var inputImgLink = this.getView().byId('imgTile').getBackgroundImage();
+
 			this.getView().byId('choices').setVisible(false);
+
+			/////DEEPAI/////
+			deepai.setApiKey('8d8f1046-9f13-4f0f-9bbd-9baa9a3246fe');
+
+			(async function() {
+				var resp = await deepai.callStandardApi("fast-style-transfer", {
+						content: inputImgLink,
+						style: inputStyleLink,
+				});
+				console.log(resp);
+				this.getView().byId('processedImg').setSrc(resp.output_url);
+				this.getView().byId('after').setVisible(true);
+			}).bind(this)()
+			/////DEEPAI/////	
+			
 			//change file name to processed img			
-			this.getView().byId('processedImg').setSrc('resources/imgprocessed/processed.jpg');
-			this.getView().byId('after').setVisible(true);
+			//this.getView().byId('processedImg').setSrc('resources/imgprocessed/processed.jpg');
+			//this.getView().byId('after').setVisible(true);
+			
+
 		},
 
 		onDownload: function(){
